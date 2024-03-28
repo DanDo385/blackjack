134429899da092202d559ad69e3d49c37bae8367
Daniel Magro <dando385@yahoo.com>
2024-03-28 15:16:27 -0400

    added favicon again

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,16 @@
 // pages/_app.jsx
+
+import Head from 'next/head'; // Import the Head component
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Blackjack</title> // Set a default title for all pages
+        <link rel="icon" href="/favicon.ico" /> // Include the favicon
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </>
@@ -12,4 +18,3 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
-
