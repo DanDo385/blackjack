ec7cc38b45c59a7380824bb683180af698dd6598
Daniel Magro <dando385@yahoo.com>
2024-03-28 11:56:36 -0400

    changed navbar image

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,18 +7,18 @@ const Navbar = () => {
     <nav className="bg-green-400 text-slate-900 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-lg font-bold flex items-center">
-          <Image src="/images/aceking.jpg" alt="Logo" width={50} height={50} className="mr-2"/>
+          <Image src="/images/acekingbw.jpg" alt="Logo" width={50} height={50} className="mr-2"/>
           <Link href="/">
-            <a className="cursor-pointer">Blackjack Game</a>
+            <span className="cursor-pointer">Blackjack</span>
           </Link>
         </div>
         <div className="flex gap-4">
           {/* Removed Login Link */}
           <Link href="/game">
-            <a className="cursor-pointer hover:text-green-700 transition-colors">Game</a>
+            <span className="cursor-pointer hover:text-green-700 transition-colors">Game</span>
           </Link>
           <Link href="/scoreboard">
-            <a className="cursor-pointer hover:text-green-700 transition-colors">Scoreboard</a>
+            <span className="cursor-pointer hover:text-green-700 transition-colors">Scoreboard</span>
           </Link>
         </div>
       </div>
