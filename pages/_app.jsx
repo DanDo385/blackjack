d7c87c407cacff6101433a0995e1f5ce4f258efd
Daniel Magro <dando385@yahoo.com>
2024-03-28 12:01:42 -0400

    added favicon.ico

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,14 @@
-// pages/_app.jsx
+import Head from 'next/head'; // Import the Head component
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Blackjack</title> {/* Set a default title; can be overridden in individual pages */}
+        <link rel="icon" href="/favicon.ico" /> {/* Reference to the favicon */}
+      </Head>
       <Navbar />
       <div className="md:ml-60">
         <Component {...pageProps} />
