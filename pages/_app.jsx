584108cf4b60d96b9304850e1783b89af92c8b8e
Daniel Magro <dando385@yahoo.com>
2024-03-29 10:27:04 -0400

    added walletprovider functions to the entire project by wrapping _app.jsx in it

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,18 +1,19 @@
 // pages/_app.jsx
 
-import Head from 'next/head'; // Import the Head component
+import Head from 'next/head';
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
+import { WalletProvider } from '../contexts/WalletContext'; // Import the provider
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <WalletProvider> {/* Wrap the app with WalletProvider */}
       <Head>
-        <title>Blackjack</title> 
+        <title>Blackjack</title>
       </Head>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </WalletProvider>
   );
 }
 
