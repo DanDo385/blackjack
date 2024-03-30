34460580572f16d9ff0e42ccb37e1e40c44f820d
Daniel Magro <dando385@yahoo.com>
2024-03-30 15:03:58 -0400

    amending Hand component

--- components/Hand.jsx
+++ components/Hand.jsx
@@ -3,7 +3,8 @@ const Hand = ({ cards }) => {
   return (
     <div className="hand flex justify-center items-center">
       {cards.map((card, index) => (
-        <img key={index} src={`./public/images/cards/${card}.png`} alt="Card" className="h-24 m-1" />
+        // Adjusted path to correctly point to the images in the public directory
+        <img key={index} src={`/images/cards/${card}.png`} alt="Card" className="h-24 m-1" />
       ))}
     </div>
   );
